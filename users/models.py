f2843a9c6c8fd0c1fb7ceb21d57b8b2b8e5aaade
STerliakov <terlya.stas@gmail.com>
2022-10-04 00:32:39 +0300

    Fix

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as UserManager_
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -13,6 +14,44 @@ class Venue(models.Model):
 
     def __str__(self):
         return f'{self.city} ({self.full_address})'
+
+
+class UserManager(UserManager_):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email: str, password: str, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(  # type: ignore[override]
+        self, email: str, password: str, **extra_fields
+    ):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(  # type: ignore[override]
+        self, email: str, password: str, **extra_fields
+    ):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -35,6 +74,8 @@ class User(AbstractUser):
         NINTH = 9, _('9')
         TENTH = 10, _('10')
         ELEVENTH = 11, _('11')
+
+    objects = UserManager()
 
     first_name = models.CharField(
         _('First name'), max_length=127, blank=False, null=False
